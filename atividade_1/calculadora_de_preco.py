6d986f397d70c08c246ebf7ffca26bfe6780aad3
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-06-25 20:56:58 -0300

    feat: atividade 04

---
+++ atividade_1/calculadora_de_preco.py
@@ -0,0 +1,12 @@
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("--- Calculadora de Preço Total ---")
+print(f"Produto: {nome_produto}")
+print(f"Preço Unitário: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print("-" * 30)
+print(f"Preço Total: R$ {preco_total:.2f}")
